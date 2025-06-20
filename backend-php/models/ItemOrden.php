20ebd7b9e7955dbeb00f5f9a4bfdf6984dbe5e83
Fed.E <112909589+FedE-URU@users.noreply.github.com>
2025-06-20 13:52:38 -0300

    Create ItemOrden.php

---
+++ backend-php/models/ItemOrden.php
@@ -0,0 +1,9 @@
+<?php
+// backend-php/models/ItemOrden.php
+
+class ItemOrden {
+    public $producto_id;
+    public $cantidad;
+    public $precio_unitario; // Para guardar el precio al momento de la compra
+}
+?>
